53aed371a784ced40639689025897cfd9c68aa28
JamunaV <jamunavikraman@gmail.com>
2025-11-03 09:12:45 +0530

    primitive data types

---
+++ src/assignments/Assignment1.java
@@ -0,0 +1,34 @@
+package assignments;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		//Always add L in the end for long primitive data type
+				long phoneNumber=8310955144L;
+				System.out.println("Phone number is : "+phoneNumber);
+				/*
+				 * Even the number after decimal point increases,float will take take upto 6
+				 * numbers only and add F at the end
+				 */
+				
+				float latitude =10.4378211f;
+				System.out.println("Latitude is : "+latitude);
+				
+				int marks=100;
+				System.out.println("Marks obtained is :"+marks);
+				
+				//boolean is primitive data type used to store the result of a condition either true or false
+				boolean flag =false;
+				char grade='A';
+				char gradeascii=65;
+				System.out.println("Grade is "+grade);
+				System.out.println("gradeascii is "+gradeascii);
+				
+				//Non-primitive data types
+				//String is a data type which is used to store collection of characters
+				String name="JAMUNA";
+				System.out.println("My name is :"+name);
+
+	}
+
+}
