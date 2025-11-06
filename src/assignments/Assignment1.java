2a3d3369ccd22096b344faccd696b044892f3854
JamunaV <jamunavikraman@gmail.com>
2025-11-06 10:19:36 +0530

    Assignment1-datatypes

--- src/assignments/Assignment1.java
+++ src/assignments/Assignment1.java
@@ -3,31 +3,116 @@ package assignments;
 public class Assignment1 {
 
 	public static void main(String[] args) {
-		//Always add L in the end for long primitive data type
-				long phoneNumber=8310955144L;
-				System.out.println("Phone number is : "+phoneNumber);
-				/*
-				 * Even the number after decimal point increases,float will take take upto 6
-				 * numbers only and add F at the end
-				 */
-				
-				float latitude =10.4378211f;
-				System.out.println("Latitude is : "+latitude);
-				
-				int marks=100;
-				System.out.println("Marks obtained is :"+marks);
-				
-				//boolean is primitive data type used to store the result of a condition either true or false
-				boolean flag =false;
-				char grade='A';
-				char gradeascii=65;
-				System.out.println("Grade is "+grade);
-				System.out.println("gradeascii is "+gradeascii);
-				
-				//Non-primitive data types
-				//String is a data type which is used to store collection of characters
-				String name="JAMUNA";
-				System.out.println("My name is :"+name);
+
+		// 1. Temperature of a city in degrees Celsius: 25.5
+		float temperature=25.5f;
+		System.out.println("Temperature of a city is :"+temperature);
+
+		// 2. Whether a customer has placed an order: true or false
+		boolean custAccept = true;
+		boolean custReject = false;
+		System.out.println("Customer Accepted  : " + custAccept);
+		System.out.println("Customer rejected  : " + custReject);
+
+		// 3. Person's phone number: "123-456-7890"
+		String custPhoneNumber = "123-456-7890";
+		System.out.println("Customer phone number is  : " + custPhoneNumber);
+
+		// 4.Amount of money in a customer's bank account: 1000.50
+		float custAmount = 1000.50f;
+		System.out.println("Customer Amount in bank is : " + custAmount);
+
+		// 5. Person's email address: "john.doe@example.com"
+		String custEmailAddress = "john.doe@example.com";
+		System.out.println("Customer email address in  : " + custEmailAddress);
+
+		// 6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		float latitude = 37.7749f;
+		float longitude = -122.4194f;
+		System.out.println("Latitude is : " + latitude);
+		System.out.println("Longitude is : " + longitude);
+		
+		// 7. Person's marital status: true or false
+		boolean custMarried = true;
+		boolean custUnmarried = false;
+		System.out.println("Customer married  : " + custMarried);
+		System.out.println("Customer unmarried  : " +custUnmarried);
+		
+		//8. Person's occupation: "Software Engineer"
+		String custOccupation = "Software Engineer";
+		System.out.println("Customer OCcupation  : " + custOccupation);
+		
+		//9. Person's favourite colour: "Blue"
+		String custFavColor = "Blue";
+		System.out.println("Customer favourite color is  : " + custFavColor);
+		
+		//10.Current year: 2023
+		short year=2023;
+		System.out.println("Current year is  : " + year);
+		
+		//11.Number of followers on a social media platform: 1,000,000
+		String followers="1,000,000";
+		System.out.println("Number of followers is  : " + followers);
+		
+		//12.Rating of a movie: 7.5
+		float movieRating=7.5f;
+		System.out.println("Movie rating is  : " + movieRating);
+		
+		//13.Person's blood type: 'A'
+		char bloodGroup='A';
+		System.out.println("Customer Blood group  : " + bloodGroup);
+		
+		//14.Title of a book: "To Kill a Mockingbird"
+		String bookTitle="To Kill a Mockingbird";
+		System.out.println("Customer Blood group  : " + bookTitle);
+		
+		//15.Number of employees in a company: 500
+		short employeeCount=500;
+		System.out.println("Employee count is  : " + employeeCount);
+		
+		//16.Time of an event: 2:30 PM
+		String eventTime="2:30 PM";
+		System.out.println("Time of an event:"+eventTime);
+		
+		//17.Name of a country: "United States"
+		String country="2:30 PM";
+		System.out.println("Name of the country is :"+country);
+		
+		//18.Person's eye color: "Brown"
+		String eyecolor="Brown";
+		System.out.println("Person's eye color :"+eyecolor);
+	
+		//19.Person's birthplace: "New York City"	
+		String birthPlace="New York City";
+		System.out.println("Person's birthplace: "+birthPlace);
+			
+		//20.Distance between two cities: 200.5
+		float distance=200.5f;
+		System.out.println("Distance between two cities is :"+distance);
+		
+
+		/*
+		 * //Always add L in the end for long primitive data type long
+		 * phoneNumber=8310955144L;
+		 * System.out.println("Phone number is : "+phoneNumber);
+		 * 
+		 * Even the number after decimal point increases,float will take take upto 6
+		 * numbers only and add F at the end
+		 * 
+		 * 
+		 * float latitude =10.4378211f; System.out.println("Latitude is : "+latitude);
+		 * 
+		 * int marks=100; System.out.println("Marks obtained is :"+marks);
+		 * 
+		 * //boolean is primitive data type used to store the result of a condition
+		 * either true or false boolean flag =false; char grade='A'; char gradeascii=65;
+		 * System.out.println("Grade is "+grade);
+		 * System.out.println("gradeascii is "+gradeascii);
+		 * 
+		 * //Non-primitive data types //String is a data type which is used to store
+		 * collection of characters String name="JAMUNA";
+		 * System.out.println("My name is :"+name);
+		 */
 
 	}
 
